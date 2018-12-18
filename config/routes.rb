cbc1d52fe598e6d5aa16c650ae1f6dacd17e5ea6
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-18 16:57:41 -0700

    renames login_controller, login view template and root path to refer to home, adds remaining oauth relevant routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
-  root 'login#index'
-  
+  root 'home#index'
+  get 'login', to: redirect('/auth/google_oauth2'), as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'topics', to: 'topics#index'
 end
