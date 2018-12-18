45a8adc52a9a7a462dca19e1d335f79dc12368cb
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-18 16:44:17 -0700

    adds login view template

--- spec/features/user_logs_in_via_google_oauth_spec.rb
+++ spec/features/user_logs_in_via_google_oauth_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'User can login' do 
-  it 'via google aouth' do 
-    
+  it 'via google aouth' do
     Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
     
     visit '/'
     click_on 'Login with Google'
+    
     expect(current_path).to eq('/topics')
   end 
 end
-    
