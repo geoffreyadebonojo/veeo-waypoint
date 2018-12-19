3bb005b51c9d0be754d47bcf8400fbfd0a529e65
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-18 17:10:38 -0700

    adds current_user authentication logic to application_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,8 @@ class SessionsController < ApplicationController
   
   def create
     auth = request.env["omniauth.auth"]
-  	@user = User.find_or_create_from_auth(auth)
-  	session[:user_id] = @user.id
+  	user = User.find_or_create_from_auth(auth)
+  	session[:user_id] = user.id
   	redirect_to :topics
   end
 
