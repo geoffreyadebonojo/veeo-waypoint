d573d2909ab2dde4698c8696f700dc2473776604
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-20 11:11:08 -0700

    adds feature test for adding a topic

---
+++ spec/features/user/user_adds_topic_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature "User visiting topic index page", type: :feature do
+  it 'can add a topic' do
+
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
+
+    visit '/'
+    click_on 'Login with Google'
+
+    visit '/topics'
+    fill_in 'topic[title]', with: 'Computer Sience'
+    click_on 'Submit'
+
+    expect(current_path).to eq('/topics')
+    expect(page).to have_content('Computer Sience')
+  end
+end
