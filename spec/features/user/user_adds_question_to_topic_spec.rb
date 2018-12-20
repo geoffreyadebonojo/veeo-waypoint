5cc846fdf6e7e64756f75dd9ac67b86bda5f7079
William Fischer <william.fischer42@gmail.com>
2018-12-19 19:25:50 -0700

    Fixed question-add error

--- spec/features/user/user_adds_question_to_topic_spec.rb
+++ spec/features/user/user_adds_question_to_topic_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 RSpec.feature "User visiting topic index page", type: :feature do
   it 'can add a question to a specific topic' do
-    user = create(:user)
-    topic = create(:topic, user: user)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
+
+    visit '/'
+    click_on 'Login with Google'
+
+    user = User.last
+    topic = create(:topic, user: user)
 
     visit '/topics'
     within(first(".topic")) do
