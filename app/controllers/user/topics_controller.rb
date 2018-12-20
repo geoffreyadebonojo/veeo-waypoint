86593fa78e316b6efa42a869985b3cad124ee0b6
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-20 11:08:38 -0700

    adds create method for topic"

--- app/controllers/user/topics_controller.rb
+++ app/controllers/user/topics_controller.rb
@@ -2,6 +2,8 @@ class User::TopicsController < ApplicationController
   before_action :authenticate
 
   def create
+    @topic = current_user.topics.create(topic_params)
+    redirect_to topics_path
   end
 
   def index
@@ -9,5 +11,11 @@ class User::TopicsController < ApplicationController
   end
 
   def show
+  end
+  
+  private 
+  
+  def topic_params
+    params.require(:topic).permit(:title)
   end
 end
