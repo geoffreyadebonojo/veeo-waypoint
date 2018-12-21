20f0446521d992b053f3e96fd49f6e6e112dd528
William Fischer <william.fischer42@gmail.com>
2018-12-21 16:19:22 -0700

    adds vcr to tests

--- spec/features/user/user_searches_a_question_spec.rb
+++ spec/features/user/user_searches_a_question_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "User Searches A Question", type: :feature do
   describe 'When a user searches a question' do
-    scenario 'the user sees a list of results from Bing' do
+    scenario 'the user sees a list of results from Bing', :vcr do
       stub_oauth_user
       visit '/'
 
@@ -19,7 +19,7 @@ RSpec.feature "User Searches A Question", type: :feature do
       end
       expect(current_path).to eq('/search')
       expect(page).to have_content(question.text)
-      expect(all('.results').length).to eq(10)
+      expect(all('.result').length).to eq(10)
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,7 +29,15 @@ RSpec.configure do |config|
 end
 
 VCR.configure do |config|
+  # TODO: Switch to "new episodes" after we have the configuration working
+  config.default_cassette_options = { :record => :all }
+
+  config.ignore_localhost = true
   config.allow_http_connections_when_no_cassette = true
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.filter_sensitive_data("<BING_CLIENT_ID>") { ENV['BING_CLIENT_ID'] }
 end
 
 Shoulda::Matchers.configure do |config|
