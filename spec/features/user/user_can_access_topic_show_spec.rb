1ba04673ff4401a3ebdfd1fd4a4523544b5ec350
mmbensalah <mmbensalah@gmail.com>
2018-12-23 11:22:17 -0600

    add second describe block to test that each question has a source

--- spec/features/user/user_can_access_topic_show_spec.rb
+++ spec/features/user/user_can_access_topic_show_spec.rb
@@ -1,28 +1,67 @@
 require 'rails_helper'
 
-RSpec.feature "User visiting topic index page", type: :feature do
-  it 'can click on a topic and go to show page' do
-
-    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
-
+RSpec.feature "User topic management", type: :feature do
+  before do
+    stub_oauth_user
     visit '/'
     click_on 'Login with Google'
-
-    user = User.last
-    topic = create(:topic, user: user)
-
-    visit '/topics'
-
-    within(first(".topic")) do
-      fill_in 'question[text]', with: 'What is a query'
-      click_on 'Save'
-    end
-
-    click_on topic.title
-
-    expect(current_path).to eq("/topics/#{topic.id}")
-
-    expect(page).to have_content(topic.title)
-    expect(page).to have_content(topic.questions.first.text)
+    create(:topic, user: User.last)
   end
+  let(:topic) { Topic.last }
+
+  describe 'user visiting topic index page' do
+    it 'can click on a topic and go to show page' do
+
+      visit '/topics'
+
+      within(first(".topic")) do
+        fill_in 'question[text]', with: 'What is a query'
+        click_on 'Save'
+      end
+
+      click_on topic.title
+
+      expect(current_path).to eq("/topics/#{topic.id}")
+
+      expect(page).to have_content(topic.title)
+      expect(page).to have_content(topic.questions.first.text)
+    end
+  end
+
+  describe 'user visiting topic show page' do
+    it 'sees each question' do
+      questions = create_list(:question, 2, topic: topic)
+      source_1  = create(:source, question: questions[0])
+      source_2  = create(:source, question: questions[1])
+
+      visit "/topics/#{topic.id}"
+
+      within(first(".question")) do
+        expect(page).to have_content(topic.questions.first.text)
+        expect(page).to have_content(source_1.url)
+      end
+
+      within(all(".question")[1]) do
+        expect(page).to have_content(topic.questions.last.text)
+        expect(page).to have_content(source_2.url)
+      end
+    end
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
