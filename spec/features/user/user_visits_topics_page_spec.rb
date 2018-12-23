370077b44cdd3683bf8a5d6b0313bb02be4594cf
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-23 13:21:34 -0700

    adds feature test for editing a topic

---
+++ spec/features/user/user_visits_topics_page_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+# As a User
+# When I visit my dashboard
+# I see a list of existing topics
+# and for each topic, I see a prompt to add new questions
+# When I click on a topic, I am taken to its show page
+
+RSpec.describe "User visiting the topics path" do
+  it "can see user's topics" do
+    user = create(:user)
+    topic_1 = create(:topic, user:user)
+    topic_2 = create(:topic, user:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit '/topics'
+
+    expect(page).to have_current_path(topics_path)
+    expect(page).to have_content(topic_1.title)
+    expect(page).to have_content(topic_2.title)
+  end
+end
