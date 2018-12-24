f746eacbd7c7beda6748051c014101d01421f1ca
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-23 21:23:10 -0700

    adds feature test for deleting a topic

---
+++ spec/features/user/user_deletes_a_topic_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "User visiting topic index page", type: :feature do
+  it "can delete a topic after confirming deletion", :js => true do
+    Capybara.current_driver = :selenium_chrome
+    user  = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit '/topics'
+    fill_in 'topic[title]', with: 'Computer Sience'
+    click_on 'Save'
+    topic = user.topics.last
+    
+    expect(page).to have_content(topic.title)
+    
+    within(first('.topic')) do
+      click_button 'Delete Topic'
+    end
+  
+    # page.accept_alert "Are you sure you want to delete everything of #{topic.title}?" do
+    #   click_on 'OK'
+    # end
+    
+    expect(current_path).to eq('/topics')
+    expect(page).to_not have_content(topic.title)
+  end
+end
