b53e5b751a8a66f13f5943b13122d8a275907b80
mmbensalah <mmbensalah@gmail.com>
2018-12-27 16:42:03 -0600

    adds validation to question model and requirement to question field

--- spec/features/user/user_adds_question_to_topic_spec.rb
+++ spec/features/user/user_adds_question_to_topic_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 RSpec.feature "User visiting topic index page", type: :feature do
-  it 'can add a question to a specific topic' do
+  before do
     stub_oauth_user
     visit '/'
     click_on 'Sign in with Google'
-    
     user = User.last
     topic = create(:topic, user: user)
-
     visit '/topics'
+  end
+
+  it 'can add a question to a specific topic' do
     within(first(".topic")) do
       fill_in 'question[text]', with: 'What is a query'
       click_on 'Save'
@@ -17,5 +18,15 @@ RSpec.feature "User visiting topic index page", type: :feature do
 
     expect(current_path).to eq('/topics')
     expect(page).to have_content('What is a query')
+  end
+
+  it 'cant add a blank question' do
+    within(first(".topic")) do
+      fill_in 'question[text]', with: ''
+      click_on 'Save'
+    end
+
+    expect(current_path).to eq('/topics')
+    expect(Question.all).to eq([])
   end
 end
