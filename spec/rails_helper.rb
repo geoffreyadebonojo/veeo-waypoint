895bd539bb96840c66b74c6f1bc8d36b2c825fa1
William Fischer <william.fischer42@gmail.com>
2018-12-27 12:11:34 -0700

    Updated capybara config to fix travis errors.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,7 +15,8 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 Capybara.register_driver :selenium_chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
+  options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
 Capybara.javascript_driver = :selenium_chrome
