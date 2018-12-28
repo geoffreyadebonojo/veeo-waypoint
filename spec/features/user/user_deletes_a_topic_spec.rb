7a9cd551050310443cbe8abbe71508acd41d4c4a
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-27 19:33:02 -0700

    adds feature test for when not confirming a deleting a topic

--- spec/features/user/user_deletes_a_topic_spec.rb
+++ spec/features/user/user_deletes_a_topic_spec.rb
@@ -23,4 +23,29 @@ RSpec.feature "User visiting topic index page", type: :feature do
     expect(current_path).to eq('/topics')
     expect(page).to_not have_content(topic.title)
   end
+  
+  describe 'User dismissing deletion confirmation' do
+    it "topic does not get deleted", :js do
+      stub_oauth_user
+      visit '/'
+      click_on 'Sign in with Google'
+      sleep 0.01
+      user = User.last
+      fill_in 'topic[title]', with: 'Computer Science'
+      click_on 'Save'
+      sleep 0.01
+      topic = user.topics.last
+
+      expect(page).to have_content(topic.title)
+
+      within(first('.topic')) do
+        page.dismiss_confirm do
+          click_on 'Delete Topic'
+        end
+      end
+
+      expect(current_path).to eq('/topics')
+      expect(page).to have_content(topic.title)
+    end
+  end
 end
