87e4b53b1ab3974daf1cf65bd9121a3381a7048a
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-29 19:29:48 -0700

    adds feature test for searching a question from topic show page

--- spec/features/user/user_executes_search_for_a_question_spec.rb
+++ spec/features/user/user_executes_search_for_a_question_spec.rb
@@ -1,24 +1,40 @@
 require 'rails_helper'
 
-RSpec.feature "Topics Index Page", type: :feature do
+RSpec.feature "Question Search", type: :feature do
   before do
     stub_oauth_user
     visit '/'
     click_on 'Sign in with Google'
     create(:topic, user: User.last)
+    create(:question, topic: Topic.last)
   end
   let(:user) { User.last }
   let(:topic) { Topic.last }
+  let(:question) { Question.last }
+  
+  describe 'User visiting topics index page' do
+    it 'can execute a search for a specific question', :vcr do
+      visit '/topics' 
+      within(first('.question', visible: :hidden)) do
+        find('.search-link', visible: :hidden).click
+      end
 
-  it 'can execute a search for a specific question', :vcr do
-    question = create(:question, topic: topic)
-    visit '/topics' 
-    within(first('.question', visible: :hidden)) do
-      find('.search-link', visible: :hidden).click
+      expect(current_path).to eq('/search')
+      expect(page).to have_content(question.text)
+      expect(all('.result').length).to eq(10)
     end
-
-    expect(current_path).to eq('/search')
-    expect(page).to have_content(question.text)
-    expect(all('.result').length).to eq(10)
+  end
+  
+  describe 'User visiting topics show page' do
+    it 'can execute a search for a specific question', :vcr do
+      visit "/topics/#{topic.id}"
+      within(first('.question')) do
+        click_on 'Search'
+      end
+  
+      expect(current_path).to eq('/search')
+      expect(page).to have_content(question.text)
+      expect(all('.result').length).to eq(10)
+    end
   end
 end
