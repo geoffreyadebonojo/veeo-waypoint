5c560011bccdde1f992e948dd549e4d6a5981ef9
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-12-30 16:08:03 -0700

    added sidebar content expectation to test

--- spec/features/user/user_search_result_sidebar_spec.rb
+++ spec/features/user/user_search_result_sidebar_spec.rb
@@ -3,17 +3,19 @@ require 'rails_helper'
 RSpec.feature "Search result sidebar", type: :feature do
   describe "After User selects a search result " do
     scenario 'user sees sidebar on result show page ', :vcr do
-      user = build_stubbed(:user)
+      user = create(:user)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      topic = build_stubbed(:topic, user: user)
-      question = build_stubbed(:question, topic: topic, text: "Test Search")
+      topic = create(:topic, user: user)
+      question = create(:question, topic: topic, text: "Test Search")
       visit search_path(question: question.text)
 
-      # Stubbing target url to hit an internal route so we don't have to wait long for the frame to load.
-      allow_any_instance_of(ApplicationController).to receive(:params).and_return(target_url: topics_path)
-
       first('.search-result-link').click
-      expect_any_instance_of(ApplicationController).to receive(:render).with(sidebar_path)
+
+      expect(page).to have_css('#notes')
+      expect(page).to have_css('#questions')
+      click_on "Save"
+      visit question_path(question)
+      expect(page).to have_content("Wikipedia")
     end
   end
 end
