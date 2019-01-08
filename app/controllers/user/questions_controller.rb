cc9197cf3b140e8dfa8b91ef2f0156ac8444652f
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-01-07 19:18:17 -0700

    adds redirect to self, but still uncommented becasue it breaks a unit test

--- app/controllers/user/questions_controller.rb
+++ app/controllers/user/questions_controller.rb
@@ -6,6 +6,7 @@ class User::QuestionsController < User::ApplicationController
     valid_user = (current_user == topic.user)
     valid_user && question = topic.questions.create(question_params)
     redirect_to topics_path
+    # redirect_to request.referrer
   end
 
   def destroy
